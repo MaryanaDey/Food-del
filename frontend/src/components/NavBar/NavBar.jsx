f499419632a4e372b3fefc70402ed67e18a4c316
Maryana Dey <Dey.maryana1989@gmail.com>
2024-04-17 19:45:34 +0200

    add NabBar

--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 import "./NavBar.css";
+import { assets } from "../../assets/assets";
 
 const NavBar = () => {
-  return <div>NavBar</div>;
+  return (
+    <div className="navbar">
+      <img src={assets.logo} alt="" />
+    </div>
+  );
 };
 
 export default NavBar;
